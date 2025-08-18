dd1d2cb779de0a5d45bf4ae0c1b7ece9f8f10828
Abiel Isak <abizmo99@gmail.com>
2025-08-19 01:34:35 +0200

    Remove cors origin array and adjust refreshtoken setup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -333,8 +333,8 @@ router.post("/login", loginLimiter, async (req, res) => {
 
         res.cookie("token", refreshToken, {
           httpOnly: true,
-          secure: true,
-          sameSite: "None",
+          secure:  process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
           path: '/',
           maxAge: 7 * 24 * 60 * 60 * 1000,
         });
